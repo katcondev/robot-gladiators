6041ada8a798d52a122c70efc86dc7f06c7861ba
KatDev <kat.contreras@gmail.com>
2021-11-03 11:55:56 -0700

    testing vanilla js funtioncs

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,4 +1,14 @@
-window.prompt("What is your robot's name?")
+
+//creates variable from user input
+var playerName =  window.prompt("What is your robot's name?")
+
+// what is this?
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message")
+//this will do math and log 20
+console.log(10+10);
+//what is this?
+console.log("Our robot's name is " + playerName);
 
 //this creates a function named "fight"
 
@@ -6,6 +16,13 @@ function fight() {
     window.alert("The fight has begun!")
 }
 
-fight();
+//fight();
 
+var playerName = "Tony the Robot";
 
+// Tony the Robot is ready for battle!
+console.log("Tony the Robot" + " is ready for battle!");
+
+console.log(playerName + " is ready for battle!");
+
+console.log(" Your Robot " + playerName + ", has won!");
