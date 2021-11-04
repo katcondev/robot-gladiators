56bf6b7072697f2b1668d827f220e6c351fc1a1c
KatDev <kat.contreras@gmail.com>
2021-11-04 13:35:21 -0700

    rounds

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,16 @@
-
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-var enemyName = ["Roborto", "Amy Android", "Robo Trumble"];
+var enemyNames = ["Roboto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var enemyHealth = 50;
-var enemyAttack = 12;
+console.log(enemyNames);
+console.log(enemyNames.length);
+console.log(enemyNames[0]);
+console.log(enemyNames[3]);
 
 var fight = function(enemyName) {
   while (playerHealth > 0 && enemyHealth > 0) {
@@ -65,14 +66,16 @@ var fight = function(enemyName) {
       window.alert(playerName + ' still has ' + playerHealth + ' health left.');
     }
   }
-}
+};
+// fight each enemy-robot by looping over them and fighting them one at a time
+for (var i = 0; i < enemyNames.length; i++) {
 
 if (playerHealth > 0) {
   // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
-  window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+  window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
 
   // pick new enemy to fight based on the index of the enemyNames array
-  var pickedEnemyName = enemyName[i];
+  var pickedEnemyName = enemyNames[i];
 
   // reset enemyHealth before starting new fight
   enemyHealth = 50;
@@ -87,9 +90,4 @@ else {
   window.alert("You have lost your robot in battle! Game Over!");
   break;
 }
-
-for (var i = 0; i < enemyName.length; i++) {
-  var pickedEnemyName = enemyName[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
 }
